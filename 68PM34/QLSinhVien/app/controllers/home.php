c0843999fbdcb97160659c61bf5831b027f53e98
nguyenkien130505 <nguyennhukien789@gmail.com>
2026-05-14 22:02:01 +0700

    URL Process
    URL Process

---
+++ 68PM34/QLSinhVien/app/controllers/home.php
@@ -0,0 +1,26 @@
+<?php
+class home
+{
+    
+    public function sinhvien($id = null)
+    {
+        
+            echo "Danh sách tất cả sinh viên";
+        
+    }
+
+    
+    public function lophoc($id = null)
+    {
+      
+            echo "Danh sách tất cả lớp học";
+    }
+        
+        
+    public function index()
+    {
+        echo "Trang chủ - Hệ thống quản lý sinh viên";
+    }
+}
+?>
+?>
--- 68PM34/QLSinhVien/public/index.php
+++ 68PM34/QLSinhVien/public/index.php
@@ -1,6 +1,4 @@
 <?php
 require_once "../app/core/App.php";
 $app = new App();
-echo "\n";
-echo "hello";
 ?>
